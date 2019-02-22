9c284f9b54840c9cbed100d5370cce2d5e147249
Alvin Huang <alvin.huang@ringcentral.com>
2019-02-22 14:10:49 +0800

    fix array element product ut

--- src/level-1/ArrayElementProduct/__test__/ArrayElementProduct.test.js
+++ src/level-1/ArrayElementProduct/__test__/ArrayElementProduct.test.js
@@ -2,7 +2,7 @@ import { productOfArrayExceptSelf } from '../ArrayElementProduct';
 
 const firstArray = [2, 2, 4, 1];
 const secondArray = [1, 0, 0, 2];
-const thirdArray = [-2, -2, -3, 2];
+const thirdArray = [-2, -2];
 describe('productExceptSelf()', () => {
   it('should return [8,8,4,16] while get [2,2,4,1]', () => {
     expect(productOfArrayExceptSelf(firstArray)).toMatchObject([8,8,4,16])
@@ -10,7 +10,7 @@ describe('productExceptSelf()', () => {
   it('should return [0,0,0,0] while get [1, 0, 0, 2]', () => {
     expect(productOfArrayExceptSelf(secondArray)).toMatchObject([0,0,0,0])
   });
-  it('should return [12, 12, 8, -12] while get [-2, -2, -3, 2]', () => {
-    expect(productOfArrayExceptSelf(thirdArray)).toMatchObject([12, 12, 8, -12])
+  it('should return [-2, -2] while get [-2, -2]', () => {
+    expect(productOfArrayExceptSelf(thirdArray)).toMatchObject([-2, -2])
   });
 });
